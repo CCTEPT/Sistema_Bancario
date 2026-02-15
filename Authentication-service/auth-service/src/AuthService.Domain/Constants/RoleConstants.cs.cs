9718d446e0a9d33747e2cd65c4df8da3ff7e7658
ecatalan-2024133 <ecatalan-2024133@kinal.edu.gt>
2026-02-15 17:41:00 -0600

    feat: Añadir roles en RoleContants.cs

--- Authentication-service/auth-service/src/AuthService.Domain/Constants/RoleConstants.cs.cs
+++ Authentication-service/auth-service/src/AuthService.Domain/Constants/RoleConstants.cs.cs
@@ -4,5 +4,9 @@ namespace AuthService.Domain.Constants;
 
 public class RoleConstants
 {
+    public const string USER_ROLE = "USER_ROLE";
+    public const string ADMIN_ROLE = "ADMIN_ROLE";
+    public const string EMPLOYEE_ROLE = "EMPLOYEE_ROLE";
 
+    public static readonly string[]AllowedRoles = [USER_ROLE, ADMIN_ROLE, EMPLOYEE_ROLE];
 }
