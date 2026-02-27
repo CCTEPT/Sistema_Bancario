45c9abc57dd6dfeac5ba082c11b08bda63412fc3
despinoza-2021308 <despinoza-2021308@kinal.edu.gt>
2026-02-27 10:10:01 -0600

    Implementacion de retiros asociado a movimientos

--- Bank-service/src/controllers/movement.controller.js
+++ Bank-service/src/controllers/movement.controller.js
@@ -3,27 +3,34 @@ import * as movementService from '../services/movement.service.js'
 export async function depositController(request, reply) {
     try {
         const movement = await movementService.deposit(
-        request.body,
-        request.user.sub
+            request.body,
+            request.user.sub
         )
-
         return reply.code(201).send(movement)
-
     } catch (error) {
         return reply.code(400).send({ message: error.message })
     }
+}
+
+export async function withdrawController(request, reply) {
+    try {
+        const movement = await movementService.withdraw(
+            request.body,
+            request.user.sub
+        );
+        return reply.code(201).send(movement);
+    } catch (error) {
+        return reply.code(400).send({ message: error.message });
     }
+}
 
-
-    export async function transferController(request, reply) {
+export async function transferController(request, reply) {
     try {
         const movement = await movementService.transfer(
-        request.body,
-        request.user.sub
+            request.body,
+            request.user.sub
         )
-
         return reply.code(201).send(movement)
-
     } catch (error) {
         return reply.code(400).send({ message: error.message })
     }
